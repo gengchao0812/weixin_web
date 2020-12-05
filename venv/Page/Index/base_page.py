640dd81a6078a309e63048e8185d05e70a5614d6
gengchao812 <gengchao0812@126.com>
2020-12-06 01:00:44 +0800

    2020年12月3日 增加人员规模取随机数 增加企业名称取pytest.mark.parmetrize参数 增加allure
    2020年12月5日
    封装方法 base_page
    改写driver从base_page读
    增加浏览器复用
    增加可能出现的报错点try carch 和截图（文件名为案例名+时间）

--- venv/Page/Index/base_page.py
+++ venv/Page/Index/base_page.py
@@ -17,7 +17,7 @@ class BasePage():
         if driver is None:
             chrome_options = Options()
             # 和浏览器打开的调试端口进行通信
-            # 浏览器要使用chrome --remote-debugging-port=9222 开启调试
+            # 浏览器要使用Chrome --remote-debugging-port=9222 开启调试
             chrome_options.debugger_address = "127.0.0.1:9222"
             self._driver = webdriver.Chrome(options=chrome_options)
             self._driver.implicitly_wait(3)
